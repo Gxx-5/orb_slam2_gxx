0f9810ad62c65526716757191d455c35fbf61095
Gxx-5 <943658034@qq.com>
2021-03-13 21:18:03 +0800

    init

---
+++ run_kitti_mono.sh
@@ -0,0 +1,2 @@
+# ./mono_kitti path_to_vocabulary path_to_settings path_to_sequence
+./Examples/Monocular/mono_kitti Vocabulary/ORBvoc.txt Examples/Monocular/KITTI00-02.yaml ../1Dataset/data_odometry_gray/dataset/sequences/00/
---
+++ run_kitti_stereo.sh
@@ -0,0 +1,2 @@
+# ./mono_kitti path_to_vocabulary path_to_settings path_to_sequence
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI00-02.yaml ../1Dataset/data_odometry_gray/dataset/sequences/00/
